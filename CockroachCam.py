9f0b218f65c9fabb9870da6a58a36115a88a051b
acaciatang <tst116@ic.ac.uk>
2023-12-22 19:46:44 -0600

    placeholder

---
+++ CockroachCam.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+#Take in video live from webcam
+cap = cv2.VideoCapture(0)
+#check that camera can be used
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True: #Keep running forever
+    #Read in a frame
+    ret, frame = cap.read()
+    #If frame is not read properly, stop
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    cv2.putText(frame,'testing',(10,500), cv2.FONT_HERSHEY_SIMPLEX, 4,(255,255,255),2,cv2.LINE_AA)
+
+    cv2.imshow('output', frame)
+
+    #Stop if any key is pressed
+    keyCode = cv2.waitKey(10)
+    print(keyCode)
+    if keyCode != 255:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
